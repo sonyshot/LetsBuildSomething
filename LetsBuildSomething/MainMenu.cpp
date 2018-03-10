45db061b397f33f971f56e6cebe1672397af7978
Gordon Weitz <sonyshot3@hotmail.com>
2018-03-09 18:10:25 -0800

    main menu looks like its working

--- LetsBuildSomething/MainMenu.cpp
+++ LetsBuildSomething/MainMenu.cpp
@@ -4,10 +4,19 @@ MainMenu::MainMenu() {
 	m_button1.setFillColor(sf::Color(255, 100, 100));
 	m_button1.setPosition(sf::Vector2f(100, 100));
 	m_button1.setSize(sf::Vector2f(50, 50));
+	m_button1.setOutlineColor(sf::Color::White);
 
 	m_button2.setFillColor(sf::Color(200, 200, 200));
 	m_button2.setPosition(sf::Vector2f(200, 100));
 	m_button2.setSize(sf::Vector2f(50, 50));
+	m_button2.setOutlineColor(sf::Color::White);
+
+	m_button3.setFillColor(sf::Color(100, 255, 100));
+	m_button3.setPosition(sf::Vector2f(300, 100));
+	m_button3.setSize(sf::Vector2f(50, 50));
+	m_button3.setOutlineColor(sf::Color::White);
+
+	m_buttons[m_currentSelection]->setOutlineThickness(5.f);
 }
 
 MainMenu::~MainMenu() {
@@ -19,6 +28,20 @@ void MainMenu::handleEvent(const sf::Event &e) {
 
 	case sf::Event::KeyPressed:
 		//change selection; select game
+		//should i add up/down support?
+		if (e.key.code == sf::Keyboard::Left) {
+			//subtract from current selection
+			//replace mod with num choices variable
+			m_buttons[m_currentSelection]->setOutlineThickness(0.f);
+			m_currentSelection = (m_currentSelection + m_numButtons - 1) % m_numButtons;
+			m_buttons[m_currentSelection]->setOutlineThickness(5.f);
+		}
+		else if (e.key.code == sf::Keyboard::Right) {
+			//add to current selection
+			m_buttons[m_currentSelection]->setOutlineThickness(0.f);
+			m_currentSelection = (m_currentSelection + 1) % m_numButtons;
+			m_buttons[m_currentSelection]->setOutlineThickness(5.f);
+		}
 		break;
 	}
 }
@@ -30,4 +53,5 @@ void MainMenu::update() {
 void MainMenu::draw(sf::RenderTarget& target, sf::RenderStates states) const {
 	target.draw(m_button1);
 	target.draw(m_button2);
+	target.draw(m_button3);
 }
--- LetsBuildSomething/MainMenu.h
+++ LetsBuildSomething/MainMenu.h
@@ -7,8 +7,14 @@
 class MainMenu : public Game {
 
 	sf::RectangleShape m_button1, m_button2, m_button3;
+	//leave empty, fill with buttons? maybe better to just hardcode since it'll be constant anyway the start
+	std::vector<sf::RectangleShape*> m_buttons = { &m_button1, &m_button2, &m_button3 };
+
+	int m_numButtons = 3;
 
 	int m_currentSelection = 0;
+
+	void chooseGame()
 
 public:
 	MainMenu();
