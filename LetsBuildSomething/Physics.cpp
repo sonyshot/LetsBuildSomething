f92ec812f4346fd35b89beda04e63702502cf438
Gordon Weitz <sonyshot3@hotmail.com>
2018-03-25 22:20:52 -0700

    progress is slow

--- LetsBuildSomething/Physics.cpp
+++ LetsBuildSomething/Physics.cpp
@@ -49,11 +49,28 @@ void Physics::translateCam(float x, float y, float z) {
 	m_cameraPos.z += z;
 }
 
-void Physics::prepVisual() {
+bool Physics::prepVisual() {
 	//do calculations and set visual objects position
+	sf::Vector3f visualPos(m_pointPos);
+	rotatePoint(visualPos, -m_cameraPhi, 1);
+	rotatePoint(visualPos, -m_cameraTheta, 2);
+	translatePoint(visualPos, -m_cameraPos.x, -m_cameraPos.y, -m_cameraPos.z);
+	sf::Vector3f screenPlane({m_cameraPos.x + m_windowDistance *(cos(m_cameraTheta)*sin(m_cameraPhi)), m_cameraPos.y + m_windowDistance * (sin(m_cameraTheta)*sin(m_cameraPhi)), m_cameraPos.z + m_windowDistance * (cos(m_cameraPhi))  });
+	if (m_windowDistance*m_windowDistance - visualPos.x*screenPlane.x - visualPos.y*screenPlane.y - visualPos.z*screenPlane.z) {
+		//point is above the plane return calcualte projection, check that its in the window, return true
+		float posDis = sqrt(visualPos.x*visualPos.x + visualPos.y*visualPos.y + visualPos.z*visualPos.z);
+		//ratio = screen distance / visualP dotted with screenplane / screen distance
+		float ratio = m_windowDistance * m_windowDistance / (visualPos.x*screenPlane.x + visualPos.y*screenPlane.y + visualPos.z*screenPlane.z);
+		//ratio * visualPos gives projection
+		m_objVisual.setPosition(sf::Vector2f(visualPos.x * ratio + 350, visualPos.z * ratio + 350));
+		return true;
+	}
+	return false;
 }
 
 Physics::Physics(StateManager* sm) {
+	m_objVisual.setFillColor(sf::Color::White);
+
 	m_stateManager = sm;
 }
 
@@ -62,21 +79,25 @@ Physics::~Physics() {
 }
 
 void Physics::update() {
-
+	m_canDraw = prepVisual();
 }
 
 void Physics::draw(sf::RenderTarget& target, sf::RenderStates states) const {
-	target.draw(m_objVisual);
+	if (m_canDraw) {
+		target.draw(m_objVisual);
+	}
 }
 
 void Physics::handleEvent(const sf::Event &e) {
 	switch (e.type) {
 	case sf::Event::KeyPressed:
 		if (e.key.code == sf::Keyboard::A) {
+			translateCam(0.f, -m_cameraSpeed, 0.f);
 			//strafe left
 		}
 		else if (e.key.code == sf::Keyboard::D) {
 			//strafe right
+			translateCam(0.f, m_cameraSpeed, 0.f);
 		}
 		else if (e.key.code == sf::Keyboard::W) {
 			//forward
@@ -89,8 +110,34 @@ void Physics::handleEvent(const sf::Event &e) {
 		else if (e.key.code == sf::Keyboard::Escape) {
 			queueSwitch(MENUSTATE);
 		}
+		else if (e.key.code == sf::Keyboard::Up) {
+			//look down
+			m_cameraPhi += 10.f*3.1415926/180.f;
+			if (m_cameraPhi > 3.1415926)
+				m_cameraPhi = 3.1415926;
+		}
+		else if (e.key.code == sf::Keyboard::Down) {
+			//look up
+			m_cameraPhi -= 10.f*3.1415926 / 180.f;
+			if (m_cameraPhi < 0)
+				m_cameraPhi = 0;
+		}
+		else if (e.key.code == sf::Keyboard::Left) {
+			//turn left
+			m_cameraTheta -= 10.f*3.1415926 / 180.f;
+		}
+		else if (e.key.code == sf::Keyboard::Right) {
+			m_cameraTheta += 10.f*3.1415926 / 180.f;
+		}
 		break;
 
+	/*case sf::Event::MouseMoved:
+		std::cout << "new mouse x: " << e.mouseMove.x << std::endl;
+		std::cout << "new mouse y: " << e.mouseMove.y << std::endl;
+		m_cameraTheta += 10*(e.mouseMove.x - 900);
+		m_cameraPhi += 10*(e.mouseMove.y - 500);
+		break;
+	*/
 	}
 	
 }
--- LetsBuildSomething/Physics.h
+++ LetsBuildSomething/Physics.h
@@ -9,19 +9,21 @@
 class Physics : public Game {
 	sf::Vector3f m_cameraPos{0.f,0.f,0.f};
 	float m_cameraTheta = 0.f;
-	float m_cameraPhi = 0.f;
+	float m_cameraPhi = 3.1415926/2.f;
 	float m_cameraYaw = 0.f;
 	float m_cameraSpeed = 20.f;
-	float m_windowDistance = 10.f;
+	float m_windowDistance = 20.f;
 	float m_windowW = 800.f;
 	float m_windowH = 800.f;
 
+	bool m_canDraw = 0;
+
 	sf::Vector3f m_pointPos{ 100.f, 25.f, 25.f };
-	sf::RectangleShape m_objVisual{ sf::Vector2f(10.f, 10.f) };
+	sf::RectangleShape m_objVisual{ sf::Vector2f(50.f, 50.f) };
 
 	void rotateCam(float angle, int dim);
 	void translateCam(float x, float y, float z);
-	void prepVisual();
+	bool prepVisual();
 
 public:
 	Physics(StateManager *);
